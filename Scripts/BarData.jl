a809c575128b3144f80613017e41969f096d5736
Patrick Chang <CHNPAT005@users.noreply.github.com>
2020-10-09 22:51:40 +0800

    Skeleton Scripts

---
+++ Scripts/BarData.jl
@@ -0,0 +1,8 @@
+## Author: Patrick Chang and Ivan Jerivich
+# Script file to look at bar data stylised facts between A2X and JSE.
+# 1) Visualise micro-price bars
+# 2) Micro-price bar returns for the 95% tails
+# 3) Micro-price bar return auto-correlation
+# Note that we investigate three time-scales: 1min, 10min and 20min
+
+## Preamble
---
+++ Scripts/TradeClassificationA2X.jl
@@ -0,0 +1,7 @@
+## Author: Patrick Chang and Ivan Jerivich
+# Script file to investigate the accuracy of rules to infer
+# the trade classification.
+# Here we only perform this for A2X data, as we have unique access
+# to the true trade classification in this case.
+
+## Preamble

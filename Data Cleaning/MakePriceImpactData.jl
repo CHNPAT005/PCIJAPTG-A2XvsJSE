ad43430331ad7bda7019f71cc455873e054de72a
Patrick Chang <CHNPAT005@users.noreply.github.com>
2020-10-11 20:13:10 +0800

    Update MakePriceImpactData.jl

--- Data Cleaning/MakePriceImpactData.jl
+++ Data Cleaning/MakePriceImpactData.jl
@@ -305,5 +305,5 @@ end
 A2X_PriceImpact = PriceImpactInfoA2X(A2X_tickers)
 JSE_PriceImpact = PriceImpactInfoJSE(JSE_tickers)
 
-save("Real Data/PriceImpactData/A2X.jld", "A2X_PriceImpact", A2X_PriceImpact)
-save("Real Data/PriceImpactData/JSE.jld", "JSE_PriceImpact", JSE_PriceImpact)
+save("Real Data/PriceImpactData/A2X_PriceImpact.jld", "A2X_PriceImpact", A2X_PriceImpact)
+save("Real Data/PriceImpactData/JSE_PriceImpact.jld", "JSE_PriceImpact", JSE_PriceImpact)
